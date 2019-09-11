42bd60625e92291b9b2f2737ec0b7f492b0d19ef
mohamedaldahoul <ma811forever@gmail.com>
2019-09-11 22:29:56 +0200

    setup the project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+import ImageGrid from './components/ImageGrid';
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <ImageGrid /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './styles.css'
+
+const Header = () => <header>List of Images</header>
+
+export default Header
